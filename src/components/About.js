fe42509c12c70f2d611d8c1d1bf9b0af8a2815e1
Yugandhar Patil <yugandhar.patil@learner.manipal.edu>
2023-02-07 15:31:30 +0530

    changes 2

--- src/components/About.js
+++ src/components/About.js
@@ -11,14 +11,15 @@ export const About = () => {
 					</h1>
 					<h3 className="who">Full-Stack Web, App and Blockchain Developer</h3>
 					<p className="about">
-						I'm currently pursuing my BTech in Mechanical Enginnering at Manipal Institute of Technology. Self-taught developer
-						specialized full-stack development, blockchain(dapps) development and app and game development. Being from a core engineering
-						branch, I upskilled myself in the IT field by learning from Youtube, Udemy, Coursea and ocassional guidance from my seniors at
-						college.
+						I'm currently pursuing my B.Tech in Mechanical Enginnering at Manipal Institute of Technology. I'm a completely self-taught
+						developer specialized full-stack development, blockchain(dapps) development and app and game development. Being from a core
+						engineering branch, I upskilled myself in the IT field by learning from Youtube, Udemy, Coursera and some ocassional guidance
+						from my seniors at college whom I'm greatly thankful to!
 						<br />
 						<br />
 						<br />
-						Interested in anything challenging in the field of IT, be it software development, Sass product development etc.
+						Interested in taking up any project that challenges my skills in the field of IT, be it software development, design product
+						development, or something completely new and challenging!
 						<br />
 						<br />
 						<br />
